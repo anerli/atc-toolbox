9bbcb46902187195d0e0f50c0c7831e5e2666540
Anders <anders.erik.lie@gmail.com>
2021-10-26 17:36:59 -0500

    svr works

--- date_utils.py
+++ date_utils.py
@@ -6,6 +6,8 @@ Return a range of dates from date_from to date_to,
 with a 1-day interval.
 cal_restrict: MarketCalendar object to restrict range
 to valid_days of.
+
+Inclusive of start_date AND end_date.
 '''
 def date_range(
 	start_date: datetime, 
@@ -14,12 +16,13 @@ def date_range(
 	):
 	start_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0)
 	end_date = end_date.replace(hour=0, minute=0, second=0, microsecond=0)
+	assert start_date < end_date
 	restrict = False
 	if cal_restrict:
 		restrict = True
 		valid_days = cal_restrict.valid_days(start_date, end_date)
 	print(valid_days)
-	days = (end_date - start_date).days
+	days = (end_date - start_date).days + 1
 	dates = []
 	for n in range(days):
 		date = start_date + timedelta(days=n)
@@ -30,5 +33,6 @@ def date_range(
 	return dates
 
 
+# Example usage
 if __name__ == '__main__':
 	print(date_range(datetime(2010, 1, 1), datetime(2010, 2, 1), mcal.get_calendar('NYSE')))
--- svr_forecaster.py
+++ svr_forecaster.py
@@ -1,48 +1,63 @@
 from sklearn.svm import SVR
+from date_utils import date_range
+import pandas_market_calendars as mcal
+import pandas as pd
 
+# TODO: May want to inherit from a Forecaster base class
 class SVR_Forecaster:
-    def __init__(self, yf_df, *SVR_args, **SVR_kwargs):
-        self.df = yf_df.copy()
-        # End time index (exclusive) for train period
-        self.t_end = self.df.shape[0]
-        # Add column for integer time step
-        self.df['TimeIndex'] = [i for i in range(self.t_end)]
-        train_X = self.df[['TimeIndex']]
-        train_Y = self.df[['Adj Close']]
-        print(train_X)
-        self.model = SVR(*SVR_args, **SVR_kwargs)
-        self.model.fit(train_X, train_Y)
+	'''
+	df: Dataframe to forecast. Must have a pd.DateIndex index.
+	feature: y feature column in df to predict.
+	SVR_args: args to pass to SVR model.
+	SVR_kwargs: kwargs to pass to SVR model.
+	'''
+	def __init__(self, df, feature, *SVR_args, **SVR_kwargs):
+		self.train_start_date = df.index[0].to_pydatetime()
+		self.train_end_date = df.index[-1].to_pydatetime()
 
-    # TODO: Need to be able to covert from time index back to datetime, for plotting and such
-    # Returned predictions should be dataframes indexed by datetime
+		self.calendar = mcal.get_calendar('NYSE')
 
-    # Return predictions over the training interval
-    def predict_train(self):
-        time_indices = list(range(0, self.t_end))
-        time_indices_dim_fix = [[t] for t in time_indices]
-        return self.model.predict(time_indices_dim_fix)
+		self.y_feature_name = feature
 
+		self.x_train = pd.DataFrame(index=df.index)
+		# Column name below doesn't actually matter
+		self.x_train['Days Since Train Start'] = [(df.index[i].to_pydatetime() - df.index[0].to_pydatetime()).days for i in range(df.shape[0])]
+		self.y_train = df[[feature]]
 
-    # dt: Number of time units past the end of the training data
-    def predict(self, dt):
-        time_indices = list(range(self.df.shape[0], self.df.shape[0] + dt))
-        time_indices_dim_fix = [[t] for t in time_indices]
-        return self.model.predict(time_indices_dim_fix)
+		self.model = SVR(*SVR_args, **SVR_kwargs)
+		self.model.fit(self.x_train, self.y_train)
 
-    #def plot()
+	def predict(self, dates):
+		dates_days_from_train_start = [(date - self.train_start_date).days for date in dates]
+		x_dim_fix = [[date] for date in dates_days_from_train_start]
+		y_pred = self.model.predict(x_dim_fix)
+		#df = pd.DataFrame(index=pd.DatetimeIndex(date_range(dates[0], dates[1], self.calendar)))
+		#df['Adj Close'] = y_pred
+		df = pd.DataFrame(index=pd.DatetimeIndex(date_range(dates[0], dates[-1], self.calendar)))
+		df[self.y_feature_name] = y_pred
+		return df
 
+	def predict_range(self, start_date, end_date):
+		# Technically no reason why this can't be the case, but seems silly
+		assert start_date >= self.train_start_date
+		# TODO: Allow user to set calendar
+		dates = date_range(start_date, end_date, self.calendar)
+		return self.predict(dates)
+
+	def predict_train(self):
+		return self.predict_range(self.train_start_date, self.train_end_date)
+		
 if __name__ == '__main__':
-    import yfinance as yf
-    import matplotlib.pyplot as plt
+	import yfinance as yf
+	import matplotlib.pyplot as plt
 
-    df = yf.download('MSFT', '2015-01-01', '2020-01-01')
+	df = yf.download('MSFT', '2015-01-01', '2020-01-01')
 
-    forecaster = SVR_Forecaster(df)
-    print(forecaster.df)
-    y_pred = forecaster.predict_train()
-    print(y_pred)
+	forecaster = SVR_Forecaster(df, 'Adj Close')
+	#print(forecaster.df)
+	y_pred = forecaster.predict_train()
+	print(y_pred)
 
-    plt.plot(df['Adj Close'])
-    plt.plot(y_pred)
-    plt.show()
-    #print(forecaster.predict(10))
+	plt.plot(df['Adj Close'])
+	plt.plot(y_pred)
+	plt.show()
