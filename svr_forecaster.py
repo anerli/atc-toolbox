6569b07bfeb4a7d3d6d54b1447fc864c3cf22be0
Anders <anders.erik.lie@gmail.com>
2021-10-26 18:14:38 -0500

    cleanup

--- svr_forecaster.py
+++ svr_forecaster.py
@@ -33,21 +33,13 @@ class SVR_Forecaster:
 		dates_days_from_train_start = [(date - self.train_start_date).days for date in dates]
 		x_dim_fix = [[date] for date in dates_days_from_train_start]
 		y_pred = self.model.predict(x_dim_fix)
-		#df = pd.DataFrame(index=pd.DatetimeIndex(date_range(dates[0], dates[1], self.calendar)))
-		#df['Adj Close'] = y_pred
-
-		#df = pd.DataFrame(index=pd.DatetimeIndex(date_range(dates[0], dates[-1], self.calendar)))
-		#df[self.y_feature_name] = y_pred
-		#return df
 
 		df = pd.DataFrame(
 			data=y_pred,
 			index=pd.DatetimeIndex(date_range(dates[0], dates[-1], self.calendar)),
 			columns=self.y_feature_names
 			)
-		#df[self.y_feature_name] = y_pred
 		return df
-		#return y_pred
 
 	def predict_range(self, start_date, end_date):
 		dates = date_range(start_date, end_date, self.calendar)
@@ -67,8 +59,8 @@ if __name__ == '__main__':
 	import matplotlib.pyplot as plt
 	from datetime import datetime
 
-	start_date = datetime(2015, 1, 1)
-	end_date = datetime(2020, 1, 1)
+	start_date = datetime(2001, 1, 1)
+	end_date = datetime(2010, 1, 1)
 
 	df = yf.download('MSFT', start_date, end_date)
 
@@ -77,31 +69,25 @@ if __name__ == '__main__':
 	# features = ['Adj Close', 'Open']
 	features = ['Adj Close']
 
-	train_end_date = datetime(2018, 1, 1)
+	train_end_date = datetime(2008, 1, 1)
 
 	# Train only on a subset of the data
 	df_train = df.loc[start_date:train_end_date,:]
 	df_test = df.loc[train_end_date:,:]
 	print(df_train)
 
-	forecaster = SVR_Forecaster(df_train, features)
-	#print(forecaster.df)
-	#y_pred = forecaster.predict_train()
+	# Predictive cababilities are very limited because of this kernel
+	# this is why we would like more advanced forecasting algorithms!
+	forecaster = SVR_Forecaster(df_train, features, kernel='rbf')
+
 	y_pred = forecaster.predict_range(start_date, end_date)
 	print(y_pred)
 	print(type(y_pred))
-
-	#plt.plot(df[[feature]])
-	#for feature in features:
 
 	for feature in features:
 		plt.plot(df_train[[feature]], label=f'{feature} Train')
 		plt.plot(df_test[[feature]], label=f'{feature} Test')
 		plt.plot(y_pred[[feature]], label=f'{feature} SVR Forecast')
 
-	#plt.plot(df[features])
-	#plt.plot(y_pred)
-	#df[features].plot()
-	#y_pred.plot()
 	plt.legend()
 	plt.show()
