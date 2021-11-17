c3855ebf8b0f3d61cc878319b3d087aba7f09314
Anders <anders.erik.lie@gmail.com>
2021-11-16 18:32:12 -0600

    clean

--- atc_toolbox/test_suite/runner.py
+++ atc_toolbox/test_suite/runner.py
@@ -16,37 +16,19 @@ def test_list(Strategy: type, fnames: List[str], verbose=False, **kwargs):
     for i, fname in enumerate(fnames):
         symbol, start_date, end_date = decompose_fname(fname)
         if verbose: print(f'({i+1}/{len(fnames)}) Testing on {symbol} from {start_date} to {end_date}...')
-        #try:
         stats = test_one(Strategy, fname, **kwargs)
         if not isinstance(stats, type(None)): stats_list.append(stats)
-        #except NoDataException:
-        #    continue
+
     return stats_list
 
 def test_full(Strategy: type, **kwargs):
-    # all_stats = []
-    # for fname in list_data():
-    #     all_stats.append(test_one(Strategy, fname, **kwargs))
-    # return all_stats
     return test_list(Strategy, list_data(), **kwargs)
-
-
 
 '''
 Test a Strategy against a single data table.
 '''
 def test_one(Strategy: type, fname: str, backtest_kwargs={}, run_kwargs={}, verbose=False, plot=False):
-    
-    #df = pd.read_csv(os.path.join(DATA_PATH, fname))
     df = load(fname)
-    #print(df.head())
-
-    # May be the case if, for example, a company was founded after the start date specified in the
-    # download call to yfinance.
-    # print(df.shape[0])
-    # if df.shape[0] == 0:
-    #     raise NoDataException()
-
     bt = Backtest(df, Strategy, **backtest_kwargs)
 
     try:
@@ -54,10 +36,7 @@ def test_one(Strategy: type, fname: str, backtest_kwargs={}, run_kwargs={}, verb
     except Exception as e:
         print(f'Caught {e.__class__.__name__} exception while running file {fname}, tossing out this result.')
         return
-
-    # bt_stats is a pd.Series
-    #print(type(bt_stats))
-    #print(bt_stats)
+    
     if plot: bt.plot()
     return bt_stats
 
--- examples/using_test_suite.py
+++ examples/using_test_suite.py
@@ -8,16 +8,12 @@ bt_sma = Backtest(df, SmaCross, exclusive_orders=True)
 bt_sma.run(momentum=False, window_short=10, window_long=100)
 '''
 
-# stats = test_one(
-#     SmaCross,
-#     'AAPL_2000-01-01_2021-01-01.csv',
-#     backtest_kwargs={'exclusive_orders': True},
-#     run_kwargs={'momentum': False, 'window_short': 10, 'window_long': 100},
-#     verbose=True
-# )
-
-# print(stats)
-# summarize([stats])
+stats_list = test_full(
+    SmaCross,
+    backtest_kwargs={'exclusive_orders': True},
+    run_kwargs={'momentum': False, 'window_short': 10, 'window_long': 100},
+    verbose=True
+)
 
 # stats_list = test_random(
 #     SmaCross,
@@ -27,15 +23,6 @@ bt_sma.run(momentum=False, window_short=10, window_long=100)
 #     verbose=True
 # )
 
-# summarize(stats_list)
-
-stats_list = test_full(
-    SmaCross,
-    backtest_kwargs={'exclusive_orders': True},
-    run_kwargs={'momentum': False, 'window_short': 10, 'window_long': 100},
-    verbose=True
-)
-
 # stats_list = test_symbols(
 #     SmaCross,
 #     ['OGN'],
@@ -43,6 +30,5 @@ stats_list = test_full(
 #     run_kwargs={'momentum': False, 'window_short': 10, 'window_long': 100},
 #     verbose=True
 # )
-
 
 summarize(stats_list)
