82f4e94dbfdd46d298be30610d509055d1e897be
Anders <anders.erik.lie@gmail.com>
2021-11-16 18:33:32 -0600

    clean

--- examples/using_test_suite.py
+++ examples/using_test_suite.py
@@ -1,6 +1,5 @@
-from atc_toolbox.test_suite.runner import test_full, test_one, summarize, test_random, test_symbols
+from atc_toolbox.test_suite.runner import test_full, summarize
 from atc_toolbox.strategies.crossover import SmaCross
-from backtesting import Backtest
 
 '''
 # Say you wanted parameters like this:
@@ -14,21 +13,5 @@ stats_list = test_full(
     run_kwargs={'momentum': False, 'window_short': 10, 'window_long': 100},
     verbose=True
 )
-
-# stats_list = test_random(
-#     SmaCross,
-#     5,
-#     backtest_kwargs={'exclusive_orders': True},
-#     run_kwargs={'momentum': False, 'window_short': 10, 'window_long': 100},
-#     verbose=True
-# )
-
-# stats_list = test_symbols(
-#     SmaCross,
-#     ['OGN'],
-#     backtest_kwargs={'exclusive_orders': True},
-#     run_kwargs={'momentum': False, 'window_short': 10, 'window_long': 100},
-#     verbose=True
-# )
 
 summarize(stats_list)
